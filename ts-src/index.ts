117739ad3c300e792231fc18febc0393924a6653
Paul Kukowski <paul.kukowski@webid-solutions.de>
2025-02-09 12:32:56 +0100

    Switch to TS

---
+++ ts-src/index.ts
@@ -0,0 +1,213 @@
+import * as wasm from "../pkg/wahlo";
+import {
+  ScatterGL,
+  Point2D,
+  PointMetadata,
+  RenderMode,
+  Sequence,
+} from "scatter-gl";
+
+interface Data {
+  labels: number[];
+  labelNames: string[];
+  projection: [number, number][];
+}
+
+const selectedAlgorithm = "umap";
+const jsCoordSpace = await wasm.load_coords(selectedAlgorithm);
+const coordinates: [[number, number]] =
+  await wasm.get_coordinates(jsCoordSpace);
+const labels: [[number, String]] = await wasm.get_labels(jsCoordSpace);
+
+const metadata: PointMetadata[] = [];
+const data: Data = { labels: [], labelNames: [], projection: [] };
+
+coordinates.forEach((point) => {
+  data.projection.push(point);
+});
+
+const dataPoints: Point2D[] = [];
+labels.forEach((idx, i) => {
+  const labelIndex = idx[0];
+  dataPoints.push(data.projection[i]);
+  metadata.push({
+    labelIndex,
+    label: idx[1] as string,
+  });
+});
+
+const sequences = makeSequences(dataPoints, metadata);
+const dataset = new ScatterGL.Dataset(dataPoints, metadata);
+
+let lastSelectedPoints: number[] = [];
+let renderMode = "points";
+
+const containerElement = document.getElementById("container")!;
+const messageElement = document.getElementById("messages")!;
+
+const setMessage = (message: string) => {
+  const messageStr = `🔥 ${message}`;
+  console.log(messageStr);
+  messageElement.innerHTML = messageStr;
+};
+
+const scatterGL = new ScatterGL(containerElement, {
+  onClick: (point: number | null) => {
+    setMessage(`click ${point}`);
+  },
+  onHover: (point: number | null) => {
+    setMessage(`hover ${point}`);
+  },
+  onSelect: (points: number[]) => {
+    let message = "";
+    if (points.length === 0 && lastSelectedPoints.length === 0) {
+      message = "no selection";
+    } else if (points.length === 0 && lastSelectedPoints.length > 0) {
+      message = "deselected";
+    } else if (points.length === 1) {
+      message = `selected ${points}`;
+    } else {
+      message = `selected ${points.length} points`;
+    }
+    setMessage(message);
+  },
+  renderMode: RenderMode.POINT,
+  orbitControls: {
+    zoomSpeed: 1.125,
+  },
+});
+
+scatterGL.render(dataset);
+
+document
+  .querySelectorAll<HTMLInputElement>('input[name="interactions"]')
+  .forEach((inputElement) => {
+    inputElement.addEventListener("change", () => {
+      if (inputElement.value === "pan") {
+        scatterGL.setPanMode();
+      } else if (inputElement.value === "select") {
+        scatterGL.setSelectMode();
+      }
+    });
+  });
+
+document
+  .querySelectorAll<HTMLInputElement>('input[name="render"]')
+  .forEach((inputElement) => {
+    inputElement.addEventListener("change", () => {
+      renderMode = inputElement.value;
+      if (inputElement.value === "points") {
+        scatterGL.setPointRenderMode();
+      } else if (inputElement.value === "sprites") {
+        scatterGL.setSpriteRenderMode();
+      } else if (inputElement.value === "text") {
+        scatterGL.setTextRenderMode();
+      }
+    });
+  });
+
+const hues = [...new Array(10)].map((_, i) => Math.floor((255 / 10) * i));
+
+const lightTransparentColorsByLabel = hues.map(
+  (hue) => `hsla(${hue}, 100%, 50%, 0.05)`,
+);
+const heavyTransparentColorsByLabel = hues.map(
+  (hue) => `hsla(${hue}, 100%, 50%, 0.75)`,
+);
+const opaqueColorsByLabel = hues.map((hue) => `hsla(${hue}, 100%, 60%, 1)`);
+
+document
+  .querySelectorAll<HTMLInputElement>('input[name="color"]')
+  .forEach((inputElement) => {
+    inputElement.addEventListener("change", () => {
+      if (inputElement.value === "default") {
+        scatterGL.setPointColorer(null);
+      } else if (inputElement.value === "label") {
+        scatterGL.setPointColorer((i, selectedIndices, hoverIndex) => {
+          const labelIndex = dataset.metadata![i]["labelIndex"] as number;
+          const opaque = renderMode !== "points";
+          if (opaque) {
+            return opaqueColorsByLabel[labelIndex];
+          } else {
+            if (hoverIndex === i) {
+              return "red";
+            }
+
+            // If nothing is selected, return the heavy color
+            if (selectedIndices.size === 0) {
+              return heavyTransparentColorsByLabel[labelIndex];
+            }
+            // Otherwise, keep the selected points heavy and non-selected light
+            else {
+              const isSelected = selectedIndices.has(i);
+              return isSelected
+                ? heavyTransparentColorsByLabel[labelIndex]
+                : lightTransparentColorsByLabel[labelIndex];
+            }
+          }
+        });
+      }
+    });
+  });
+
+const dimensionsToggle =
+  document.querySelector<HTMLInputElement>('input[name="3D"]')!;
+dimensionsToggle.addEventListener("change", (_: any) => {
+  const is3D = dimensionsToggle.checked;
+  scatterGL.setDimensions(is3D ? 3 : 2);
+});
+
+const sequencesToggle = document.querySelector<HTMLInputElement>(
+  'input[name="sequences"]',
+)!;
+sequencesToggle.addEventListener("change", (_: any) => {
+  const showSequences = sequencesToggle.checked;
+  scatterGL.setSequences(showSequences ? sequences : []);
+});
+
+// Set up controls for buttons
+const selectRandomButton = document.getElementById("select-random")!;
+selectRandomButton.addEventListener("click", () => {
+  const randomIndex = Math.floor(dataPoints.length * Math.random());
+  scatterGL.select([randomIndex]);
+});
+
+const toggleOrbitButton = document.getElementById("toggle-orbit")!;
+toggleOrbitButton.addEventListener("click", () => {
+  if (scatterGL.isOrbiting()) {
+    scatterGL.stopOrbitAnimation();
+  } else {
+    scatterGL.startOrbitAnimation();
+  }
+});
+
+export function makeSequences(
+  points: Point2D[],
+  metadata: PointMetadata[],
+  nSequencesPerLabel = 3,
+  sequenceLength = 5,
+) {
+  const pointIndicesByLabel = new Map<string, number[]>();
+  points.forEach((_, index) => {
+    const label = metadata[index].label!;
+    const pointIndices = pointIndicesByLabel.get(label) || [];
+    pointIndices.push(index);
+    pointIndicesByLabel.set(label, pointIndices);
+  });
+
+  const sequences: Sequence[] = [];
+
+  pointIndicesByLabel.forEach((indices) => {
+    for (let i = 0; i < nSequencesPerLabel; i++) {
+      const sequence: Sequence = { indices: [] };
+      for (let j = 0; j < sequenceLength; j++) {
+        const index = indices[i * sequenceLength + j];
+        sequence.indices.push(index);
+      }
+      sequence.indices.push(sequence.indices[0]);
+      sequences.push(sequence);
+    }
+  });
+
+  return sequences;
+}
